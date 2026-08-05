ed6f7c0c45ee99647f7c35164570b9b39d903e8b
Alireza Naseri <alirezanaseri369@gmail.com>
2026-08-05 19:21:21 +0330

    feat: add step 11 interfaces examples

---
+++ 11-interfaces/index.ts
@@ -0,0 +1,18 @@
+﻿interface Guitarist {
+  name: string
+  active: boolean
+  albums: (string | number)[]
+}
+
+const player: Guitarist = {
+  name: "Jimmy",
+  active: true,
+  albums: ["Led Zeppelin I", "Led Zeppelin II", 1971],
+}
+
+console.log(player)
+
+/*
+Lesson:
+Interfaces define object contracts and are often preferred for extensible object shapes.
+*/
