c62c158bf25e1f28794468372b4fdf8d9a0e9cbc
Alireza Naseri <alirezanaseri369@gmail.com>
2026-08-05 19:21:31 +0330

    feat: add step 13 literal types examples

---
+++ 13-literal-types/index.ts
@@ -0,0 +1,13 @@
+﻿let username: "Dave" | "John" | "Amy"
+username = "Amy"
+
+let status: "success" | "error" | "loading"
+status = "success"
+
+console.log(username)
+console.log(status)
+
+/*
+Lesson:
+Literal types restrict values to exact allowed options.
+*/
