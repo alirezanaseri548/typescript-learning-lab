23aedc0238c654e779fb50aba205bfd8d690b90e
Alireza Naseri <alirezanaseri369@gmail.com>
2026-08-05 19:21:16 +0330

    feat: add step 10 type aliases examples

---
+++ 10-type-aliases/index.ts
@@ -0,0 +1,18 @@
+﻿type Guitarist = {
+  name: string
+  active: boolean
+  albums: (string | number)[]
+}
+
+let evh: Guitarist = {
+  name: "Eddie",
+  active: false,
+  albums: [1984, 5150, "OU812"],
+}
+
+console.log(evh)
+
+/*
+Lesson:
+Type aliases improve readability and reusability for custom shapes.
+*/
