8e06617d7b82d9fbd566710381b59407372fc072
Alireza Naseri <alirezanaseri369@gmail.com>
2026-08-05 19:21:25 +0330

    feat: add step 12 enums examples

---
+++ 12-enums/index.ts
@@ -0,0 +1,15 @@
+﻿enum Grade {
+  U,
+  D,
+  C,
+  B,
+  A,
+}
+
+console.log("Grade U:", Grade.U)
+console.log("Grade A:", Grade.A)
+
+/*
+Lesson:
+Enums provide named constants and improve readability for fixed sets of values.
+*/
