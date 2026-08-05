8d67d974e093ad18375a48d7b37b8e02372e0876
Alireza Naseri <alirezanaseri369@gmail.com>
2026-08-05 19:21:11 +0330

    feat: add step 09 objects examples

---
+++ 09-objects/index.ts
@@ -0,0 +1,22 @@
+﻿const exampleObject = {
+  prop1: "Dave",
+  prop2: true,
+}
+
+const person: {
+  name: string
+  age: number
+  active: boolean
+} = {
+  name: "Alireza",
+  age: 27,
+  active: true,
+}
+
+console.log(exampleObject)
+console.log(person)
+
+/*
+Lesson:
+TypeScript can infer object property types from assigned values.
+*/
