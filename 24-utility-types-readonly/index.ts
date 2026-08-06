96394981eeccb03e5069dfd628e9b1b98212243b
Alireza Naseri <alirezanaseri369@gmail.com>
2026-08-06 21:27:10 +0330

    feat: add step 24 readonly utility type

---
+++ 24-utility-types-readonly/index.ts
@@ -0,0 +1,19 @@
+﻿type AppConfig = {
+  appName: string
+  version: string
+  debug: boolean
+}
+
+const config: Readonly<AppConfig> = {
+  appName: "TypeScript Learning Lab",
+  version: "1.0.0",
+  debug: false,
+}
+
+console.log(config)
+
+/*
+Lesson:
+Readonly<T> prevents reassignment of object properties.
+It is useful for configuration objects.
+*/
