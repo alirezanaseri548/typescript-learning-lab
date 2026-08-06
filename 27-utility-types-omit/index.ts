ca4536e0e12b5060935725c65f645e59b4022d94
Alireza Naseri <alirezanaseri369@gmail.com>
2026-08-06 21:27:35 +0330

    feat: add step 27 omit utility type

---
+++ 27-utility-types-omit/index.ts
@@ -0,0 +1,22 @@
+﻿type DatabaseUser = {
+  id: number
+  name: string
+  email: string
+  passwordHash: string
+}
+
+type SafeUser = Omit<DatabaseUser, "passwordHash">
+
+const safeUser: SafeUser = {
+  id: 1,
+  name: "Alireza",
+  email: "alirezanaseri369@gmail.com",
+}
+
+console.log(safeUser)
+
+/*
+Lesson:
+Omit<T, K> creates a new type by removing selected properties from T.
+It is useful for hiding sensitive fields.
+*/
