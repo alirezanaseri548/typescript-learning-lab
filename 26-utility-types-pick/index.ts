5bf9165c265fc6aeb65ab028cda1ef3ad59a3a88
Alireza Naseri <alirezanaseri369@gmail.com>
2026-08-06 21:27:27 +0330

    feat: add step 26 pick utility type

---
+++ 26-utility-types-pick/index.ts
@@ -0,0 +1,22 @@
+﻿type FullUser = {
+  id: number
+  name: string
+  email: string
+  passwordHash: string
+  createdAt: string
+}
+
+type PublicUser = Pick<FullUser, "id" | "name" | "email">
+
+const publicUser: PublicUser = {
+  id: 1,
+  name: "Alireza",
+  email: "alirezanaseri369@gmail.com",
+}
+
+console.log(publicUser)
+
+/*
+Lesson:
+Pick<T, K> creates a new type by selecting specific properties from T.
+*/
