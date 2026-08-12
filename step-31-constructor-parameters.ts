6a91e0cfbe3a90b3b8360f747d4a9cf54d1a5fdc
Alireza Naseri <alirezanaseri369@gmail.com>
2026-08-12 09:40:59 +0330

    Step 31: Implement ConstructorParameters Utility Type

---
+++ step-31-constructor-parameters.ts
@@ -0,0 +1,12 @@
+﻿/**
+ * STEP 31: ConstructorParameters Utility Type
+ */
+class UserSession {
+  constructor(public token: string, public expiration: number) {}
+}
+
+type SessionParams = ConstructorParameters<typeof UserSession>;
+const sessionArgs: SessionParams = ["jwt-token-xyz-123", 3600];
+const session = new UserSession(...sessionArgs);
+
+console.log("Step 31: ConstructorParameters verified. Token:", session.token);
