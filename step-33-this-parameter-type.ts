e8b305beff5a38c900f970401dea0ddb96edd19e
Alireza Naseri <alirezanaseri369@gmail.com>
2026-08-12 09:41:17 +0330

    Step 33: Implement ThisParameterType Utility Type

---
+++ step-33-this-parameter-type.ts
@@ -0,0 +1,10 @@
+﻿/**
+ * STEP 33: ThisParameterType Utility Type
+ */
+function toHex(this: Number) {
+  return this.toString(16);
+}
+
+type ThisTypeHex = ThisParameterType<typeof toHex>;
+const numberContext: ThisTypeHex = 255;
+console.log("Step 33: ThisParameterType verified. Context:", numberContext);
