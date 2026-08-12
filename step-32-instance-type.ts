187b2189a3099962b0eab6800b5c19beb1cf747b
Alireza Naseri <alirezanaseri369@gmail.com>
2026-08-12 09:41:08 +0330

    Step 32: Implement InstanceType Utility Type

---
+++ step-32-instance-type.ts
@@ -0,0 +1,12 @@
+﻿/**
+ * STEP 32: InstanceType Utility Type
+ */
+class HttpService {
+  request(url: string) {
+    return `Fetching data from ${url}`;
+  }
+}
+
+type HttpServiceInstance = InstanceType<typeof HttpService>;
+const clientService: HttpServiceInstance = new HttpService();
+console.log("Step 32: InstanceType verified:", clientService.request("https://api.github.com"));
