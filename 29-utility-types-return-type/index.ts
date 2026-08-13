0e27868f9d1e6c70c580b2581feb809534626f04
Alireza Naseri <alirezanaseri369@gmail.com>
2026-08-13 19:53:39 +0330

    Add step 29 ReturnType utility type

---
+++ 29-utility-types-return-type/index.ts
@@ -0,0 +1,19 @@
+﻿type User = {
+  id: number;
+  name: string;
+  isAdmin: boolean;
+};
+
+function createUser(): User {
+  return {
+    id: 1,
+    name: "Alireza",
+    isAdmin: false
+  };
+}
+
+type CreatedUser = ReturnType<typeof createUser>;
+
+const user: CreatedUser = createUser();
+
+console.log(user);
