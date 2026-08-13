619f1e4c3b7a3ba317a95d72a177bac16b5bb7fd
Alireza Naseri <alirezanaseri369@gmail.com>
2026-08-13 19:53:57 +0330

    Add step 34 OmitThisParameter utility type

---
+++ 34-utility-types-omit-this-parameter/index.ts
@@ -0,0 +1,9 @@
+﻿function greet(this: { name: string }, message: string) {
+  return message + ", " + this.name;
+}
+
+const user = { name: "Alireza" };
+
+const boundGreet: OmitThisParameter<typeof greet> = greet.bind(user);
+
+console.log(boundGreet("Hello"));
