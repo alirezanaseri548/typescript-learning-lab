2875d00f8a0b8d874e8e14e4a4f25a452dab3f39
Alireza Naseri <alirezanaseri369@gmail.com>
2026-08-13 19:53:49 +0330

    Add step 32 InstanceType utility type

---
+++ 32-utility-types-instance-type/index.ts
@@ -0,0 +1,12 @@
+﻿class Car {
+  constructor(
+    public brand: string,
+    public year: number
+  ) {}
+}
+
+type CarInstance = InstanceType<typeof Car>;
+
+const myCar: CarInstance = new Car("Toyota", 2020);
+
+console.log(myCar);
