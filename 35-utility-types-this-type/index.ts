0be4fb3d3d53310eaef5441447021a13500c9fcd
Alireza Naseri <alirezanaseri369@gmail.com>
2026-08-13 19:54:02 +0330

    Add step 35 ThisType utility type

---
+++ 35-utility-types-this-type/index.ts
@@ -0,0 +1,25 @@
+﻿type ObjectDescriptor<D, M> = {
+  data?: D;
+  methods?: M & ThisType<D & M>;
+};
+
+function makeObject<D, M>(desc: ObjectDescriptor<D, M>): D & M {
+  return {
+    ...(desc.data || {}),
+    ...(desc.methods || {})
+  } as D & M;
+}
+
+const obj = makeObject({
+  data: {
+    name: "Alireza",
+    age: 27
+  },
+  methods: {
+    introduce() {
+      return "I am " + this.name + " and I am " + this.age + " years old.";
+    }
+  }
+});
+
+console.log(obj.introduce());
