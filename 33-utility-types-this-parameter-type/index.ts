73ff22cb8969fbe60efb84fe73f64f2ae9086629
Alireza Naseri <alirezanaseri369@gmail.com>
2026-08-13 19:53:53 +0330

    Add step 33 ThisParameterType utility type

---
+++ 33-utility-types-this-parameter-type/index.ts
@@ -0,0 +1,12 @@
+﻿function printUser(this: { name: string; age: number }, prefix: string) {
+  console.log(prefix + " " + this.name + " (" + this.age + ")");
+}
+
+type PrintUserThis = ThisParameterType<typeof printUser>;
+
+const currentUser: PrintUserThis = {
+  name: "Alireza",
+  age: 27
+};
+
+printUser.call(currentUser, "User:");
