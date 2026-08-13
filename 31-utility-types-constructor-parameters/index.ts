fd3b9289779be35a0212b1394028af2c8d84a18c
Alireza Naseri <alirezanaseri369@gmail.com>
2026-08-13 19:53:46 +0330

    Add step 31 ConstructorParameters utility type

---
+++ 31-utility-types-constructor-parameters/index.ts
@@ -0,0 +1,15 @@
+﻿class Product {
+  constructor(
+    public title: string,
+    public price: number,
+    public inStock: boolean
+  ) {}
+}
+
+type ProductConstructorParams = ConstructorParameters<typeof Product>;
+
+const productArgs: ProductConstructorParams = ["Keyboard", 2500, true];
+
+const product = new Product(...productArgs);
+
+console.log(product);
