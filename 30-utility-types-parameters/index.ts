b8d2959dc4a0236ef46e112a3ff46b9f9b0419fa
Alireza Naseri <alirezanaseri369@gmail.com>
2026-08-13 19:53:43 +0330

    Add step 30 Parameters utility type

---
+++ 30-utility-types-parameters/index.ts
@@ -0,0 +1,15 @@
+﻿function createAccount(username: string, age: number, isActive: boolean) {
+  return {
+    username,
+    age,
+    isActive
+  };
+}
+
+type CreateAccountParams = Parameters<typeof createAccount>;
+
+const args: CreateAccountParams = ["alireza", 27, true];
+
+const account = createAccount(...args);
+
+console.log(account);
